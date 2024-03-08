53ac666acb690df67d50d73b8b60273459123b80
n5945 <nicole.5945@aluno.pr.senac.br>
2024-03-08 11:41:12 -0300

    Criado o UserController

---
+++ src/controller/UserController.js
@@ -0,0 +1,25 @@
+const knex = require("../database/knex")
+
+class UserController {
+   
+async createUser(req, res) {
+    const {name, email, fone} = req.body
+
+    const books = false
+    const loan = false
+
+    await knex("users").insert({name, email, fone})
+
+    res.status(201).json("Usuario cadastrado com sucesso.")
+}
+
+async listUser(req, res) {
+    const users = await knex("users")
+
+    res.status(200).json(users)
+
+}
+
+}
+
+module.exports = UserController
